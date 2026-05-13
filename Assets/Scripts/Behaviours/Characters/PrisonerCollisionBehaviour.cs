d92557baa67eb2b60f874ed0de694099dbefc22f
Lilian Sananikone <lilian.sananikone@laposte.net>
2026-05-13 18:29:02 +0200

    Add scripts to handle collision with characters

---
+++ Assets/Scripts/Behaviours/Characters/PrisonerCollisionBehaviour.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace BWW.Behaviours.Characters
+{
+   public class PrisonnerCollisionBehaviour : CharacterCollisionBehaviour
+   {
+      public override void OnTriggerEnter(Collider p_collider)
+      {
+         // We need to check the collision with villgers only
+      }
+   }
+}
---
+++ Assets/Scripts/Behaviours/Characters/WerewolfCollisionBehaviour.cs
@@ -0,0 +1,25 @@
+using BWW.Behaviours.Map.Items;
+using BWW.Managers.Map;
+using UnityEngine;
+
+namespace BWW.Behaviours.Characters
+{
+   public class WerewolfCollisionBehaviour : CharacterCollisionBehaviour
+   {
+      public override void OnTriggerEnter(Collider p_collider)
+      {
+         MovableItem l_item = p_collider.GetComponent<MovableItem>();
+
+         if (l_item != null)
+         {
+            CharacterDataBehaviour l_data = GetComponent<CharacterDataBehaviour>();
+            if (l_data.State == Enums.ECharacterState.PUSHED)
+            {
+               // The werewolf must take damages according to his speed for the distance between the place where he was hurt and the place where he is now
+            }
+
+            NavMeshManager.Instance.HandleFlag(p_collider.gameObject);
+         }
+      }
+   }
+}
