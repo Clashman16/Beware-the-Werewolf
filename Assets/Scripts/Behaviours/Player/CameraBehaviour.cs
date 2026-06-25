fbd8a635652066d45bb3664ac8e1b2494fe51b96
Clashman <lilian.sananikone@laposte.net>
2026-06-25 16:58:57 +0200

    Add customisable speed for camera movements

--- Assets/Scripts/Behaviours/Player/CameraBehaviour.cs
+++ Assets/Scripts/Behaviours/Player/CameraBehaviour.cs
@@ -6,87 +6,93 @@ using UnityEngine;
 
 namespace BWW.Behaviours.Player
 {
-   public class CameraBehaviour : MonoBehaviour
-   {
-      private const string m_sItemSelectionMask = "MovableItem";
+    public class CameraBehaviour : MonoBehaviour
+    {
+        private const string m_sItemSelectionMask = "MovableItem";
 
-      private Vector3 m_vecMovePivot;
+        private Vector3 m_vecMovePivot;
 
-      private int[] m_lstZoomLimits;
+        private int[] m_lstZoomLimits;
 
-      PlayerCameraState m_state;
+        PlayerCameraState m_state;
 
-      public PlayerCameraState State
-      {
-         get => m_state;
-         set
-         {
-            m_state = value;
+        [SerializeField] private float m_fSpeed = 50f;
 
-            m_state.IsMoving = false;
-         }
-      }
+        Camera m_camera;
 
-      private void Start()
-      {
-         m_vecMovePivot = new Vector3(18.0540009f, 0, -34.9550018f);
+        public PlayerCameraState State
+        {
+            get => m_state;
+            set
+            {
+                m_state = value;
 
-         m_lstZoomLimits = new[]{ 3, 7 };
-      }
+                m_state.IsMoving = false;
+            }
+        }
 
-      private void Update()
-      {
-         if(GridManager.Instance.SelectedCell == null)
-         {
-            m_state.UpdateState();
+        private void Start()
+        {
+            m_vecMovePivot = new Vector3(18.0540009f, 0, -34.9550018f);
+
+            m_lstZoomLimits = new[] { 3, 7 };
+
+            m_camera = GetComponent<Camera>();
+        }
+
+        private void Update()
+        {
+            if (GridManager.Instance.SelectedCell == null)
+            {
+                m_state.UpdateState();
 
             if(m_state.IsClickDown() && !m_state.IsRotatingItem)
-            {
-               Ray l_ray = GetComponent<Camera>().ScreenPointToRay(m_state.GetPointerPosition());
+                {
+                    Ray l_ray = m_camera.ScreenPointToRay(m_state.GetPointerPosition());
 
-               if (Physics.Raycast(l_ray, out RaycastHit l_hit, 100f, LayerMask.GetMask(m_sItemSelectionMask)))
-               {
-                  MovableItem l_item = l_hit.collider.GetComponent<MovableItem>();
+                    if (Physics.Raycast(l_ray, out RaycastHit l_hit, 100f, LayerMask.GetMask(m_sItemSelectionMask)))
+                    {
+                        MovableItem l_item = l_hit.collider.GetComponent<MovableItem>();
 
-                  if (l_item != null)
-                  {
-                     GridManager.Instance.SelectItemOnGrid(l_item);
-                  }
-               }
+                        if (l_item != null)
+                        {
+                            GridManager.Instance.SelectItemOnGrid(l_item);
+                        }
+                    }
+                }
             }
-         }
-         
+
          if(m_state.IsMoving)
-         {
+            {
             if(m_state.SimulatedControl == EControls.ZOOM)
-            {
-               float l_fCurrentPosition = transform.position.y;
+                {
+                    float l_fCurrentPosition = transform.position.y;
 
-               if ((m_state.IsForwardZoom && l_fCurrentPosition > m_lstZoomLimits[0])
-                  || (!m_state.IsForwardZoom && l_fCurrentPosition < m_lstZoomLimits[1]))
-               {
-                  Vector3 l_vecZoomDirection = transform.forward * (m_state.IsForwardZoom ? 1 : -1);
+                    if ((m_state.IsForwardZoom && l_fCurrentPosition > m_lstZoomLimits[0])
+                       || (!m_state.IsForwardZoom && l_fCurrentPosition < m_lstZoomLimits[1]))
+                    {
+                        Vector3 l_vecZoomDirection = transform.forward * (m_state.IsForwardZoom ? 1 : -1);
 
-                  transform.Translate(l_vecZoomDirection, Space.World);
-               }
-            }
-            else
-            {
-               float l_fAngle = m_state.SimulatedControl == EControls.CAMERA_LEFT ? 1 : -1;
+                        transform.Translate(l_vecZoomDirection * Time.deltaTime * m_fSpeed, Space.World);
+                    }
+                }
+                else
+                {
+                    float l_fAngle = m_state.SimulatedControl == EControls.CAMERA_LEFT ? 1 : -1;
 
-               transform.RotateAround(m_vecMovePivot, Vector3.up, l_fAngle);
+                    transform.RotateAround(m_vecMovePivot, Vector3.up, l_fAngle * Time.deltaTime * m_fSpeed);
 
                if(m_state.IsRotatingItem)
-               {
-                  m_state.RotateCursor.Rotate(l_fAngle);
-               }
+                    {
+                        m_state.RotateCursor.Rotate(l_fAngle);
+                    }
+                }
             }
-         }
-      }
+        }
 
-      public void SetIsMoving(bool p_bIsMoving)
-      {
-         m_state.IsMoving = p_bIsMoving;
-      }
-   }
+        public void SetIsMoving(bool p_bIsMoving)
+        {
+            m_state.IsMoving = p_bIsMoving;
+        }
+    }
 }
