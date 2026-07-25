3f86a002944620d66d22fa5fcf854b8dc6e7b82c
Clashman <lilian.sananikone@laposte.net>
2026-07-25 22:14:24 +0200

    Make villager texture getter to use a constant path

--- Assets/Scripts/Utils/Characters/VillagerTextureGetter.cs
+++ Assets/Scripts/Utils/Characters/VillagerTextureGetter.cs
@@ -4,40 +4,42 @@ using UnityEngine;
 
 namespace BWW.Utils.Characters
 {
-   public sealed class VillagerTextureGetter
-   {
-      private static VillagerTextureGetter m_instance;
+    public sealed class VillagerTextureGetter
+    {
+        private static VillagerTextureGetter m_instance;
 
-      private Dictionary<string, Texture2D> m_lstTextures;
+        private Dictionary<string, Texture2D> m_lstTextures;
 
-      public static VillagerTextureGetter Instance
-      {
-         get
-         {
-            if(m_instance == null)
+        private const string m_sDatabasePath = "ScriptableObjects/Characters/VillagerTextureDatabase";
+
+        public static VillagerTextureGetter Instance
+        {
+            get
             {
-               m_instance = new VillagerTextureGetter();
+                if (m_instance == null)
+                {
+                    m_instance = new VillagerTextureGetter();
+                }
+
+                return m_instance;
             }
+        }
 
-            return m_instance;
-         }
-      }
+        private VillagerTextureGetter()
+        {
+            ScriptableVillagerTextureDatabase l_database = Resources.Load<ScriptableVillagerTextureDatabase>(m_sDatabasePath);
 
-      private VillagerTextureGetter()
-      {
-         ScriptableVillagerTextureDatabase l_database = Resources.Load<ScriptableVillagerTextureDatabase>("ScriptableObjects/Characters/VillagerTextureDatabase");
+            m_lstTextures = new Dictionary<string, Texture2D>();
 
-         m_lstTextures = new Dictionary<string, Texture2D>();
+            foreach (VillagerTexture l_tex in l_database.Textures)
+            {
+                m_lstTextures.Add(l_tex.Key, l_tex.Texture);
+            }
+        }
 
-         foreach (VillagerTexture l_tex in l_database.Textures)
-         {
-            m_lstTextures.Add(l_tex.Key, l_tex.Texture);
-         }
-      }
-
-      public Texture2D GetTextureFromKey(string p_sKey)
-      {
-         return m_lstTextures[p_sKey];
-      }
-   }
+        public Texture2D GetTextureFromKey(string p_sKey)
+        {
+            return m_lstTextures[p_sKey];
+        }
+    }
 }
